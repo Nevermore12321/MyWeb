5fe4a0555d49994b3068c5be15922478662fd79b
Nevermore12321 <842125706@qq.com>
2020-06-01 22:35:58 +0800

    learn react state and lifecycle

---
+++ client/src/component/Header.js
@@ -0,0 +1,38 @@
+import React  from "react";
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            curDate: new Date(),
+        };
+    }
+
+    componentDidMount() {
+        this.timeID = setInterval(() => {
+            this.tick();
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timeID);
+    }
+
+    tick() {
+        this.setState((state, props) => {
+            return {
+                curDate: new Date(),
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Clock</h1>
+                <h2>It is { this.state.curDate.toLocaleTimeString() }</h2>
+            </div>
+        );
+    }
+}
---
+++ client/src/component/Navigator.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navigator(props) {
+    return (
+        <div>
+            <h1>
+                navigator
+            </h1>
+        </div>
+    )
+}
